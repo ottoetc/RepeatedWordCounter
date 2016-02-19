b7c7ed13c0a7ea9d77c05471f957379e32276aef
Nathan <ottoetc@gmail.com>
2016-02-19 10:34:37 -0800

    add third test

--- Tests/RepeatCounterTests.cs
+++ Tests/RepeatCounterTests.cs
@@ -16,6 +16,12 @@ namespace CountRepeatWords
       RepeatCounter testRepeatCounter = new RepeatCounter();
       Assert.Equal(1, testRepeatCounter.CountRepeats("lorem ipsum", "lorem"));
     }
+    [Fact]
+    public void CountRepeats_Test_CountMultiple()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter();
+      Assert.Equal(4, testRepeatCounter.CountRepeats("one fish two fish red fish blue fish", "fish"));
+    }
   }
 
 }
